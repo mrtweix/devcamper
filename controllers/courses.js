8c48c4e9a38cbf03a4fd65bdde0d78aece8278bd
Barender Singh <gagan007bangs@gmail.com>
2021-05-05 22:23:07 +0530

    delete, update courses api added

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const Course = require("../models/Course");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
+const Bootcamp = require("../models/Bootcamp");
 // const asyncHandler = require("express-async-handler");
 
 // @desc      Get courses
@@ -14,14 +15,134 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   } else {
     query = Course.find().populate({
       path: "bootcamp", // model name in path to populate
-      select: "_id name description", // select particular fields of a bootcamp collection
+      select: "name description", // select particular fields of a bootcamp collection
     });
   }
-
   const course = await query;
   return res.status(200).json({
     success: true,
     count: course.length,
     data: course,
+  });
+});
+
+// @desc      Get single course
+// @route     GET /api/v1/courses/:id
+// @access    Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: "bootcamp", // model name in path to populate
+    select: "name description", // select particular fields of a bootcamp collection
+  });
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc      Add course
+// @route     POST /api/v1/bootcamps/:bootcampId/courses
+// @access    Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  console.log(req.params.bootcampId);
+  req.body.bootcamp = req.params.bootcampId;
+  // req.body.user = req.user.id;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(
+        `Found no bootcamp with the id of ${req.params.bootcampId}`,
+        404
+      )
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  // if (bootcamp.user.toString() !== req.user.id && req.user.role !== "admin") {
+  //   return next(
+  //     new ErrorResponse(
+  //       `User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+  //       401
+  //     )
+  //   );
+  // }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc      Update course
+// @route     PUT /api/v1/courses/:id
+// @access    Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is course owner
+  // if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+  //   return next(
+  //     new ErrorResponse(
+  //       `User ${req.user.id} is not authorized to update course ${course._id}`,
+  //       401
+  //     )
+  //   );
+  // }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  course.save();
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc      Delete course
+// @route     DELETE /api/v1/courses/:id
+// @access    Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is course owner
+  // if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+  //   return next(
+  //     new ErrorResponse(
+  //       `User ${req.user.id} is not authorized to delete course ${course._id}`,
+  //       401
+  //     )
+  //   );
+  // }
+
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
   });
 });
