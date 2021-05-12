a13b20f51725224d653c1e46b43838dd87024aee
Barender Singh <gagan007bangs@gmail.com>
2021-05-12 20:27:28 +0530

    auth routes & token return api

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ if (process.env.NODE_ENV === "development") {
 // Route files
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 
