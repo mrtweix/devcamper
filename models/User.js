61aa340f9a3f7a6ca66645aa73148be461e9d660
Barender Singh <gagan007bangs@gmail.com>
2021-05-16 20:56:35 +0530

    reset password api

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-// const crypto = require("crypto");
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -70,22 +70,22 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// // Generate and hash password token
-// UserSchema.methods.getResetPasswordToken = function () {
-//   // Generate token
-//   const resetToken = crypto.randomBytes(20).toString("hex");
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-//   // Hash token and set to resetPasswordToken field
-//   this.resetPasswordToken = crypto
-//     .createHash("sha256")
-//     .update(resetToken)
-//     .digest("hex");
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-//   // Set expire
-//   this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+  // Set expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
-//   return resetToken;
-// };
+  return resetToken;
+};
 
 // // Generate email confirm token
 // UserSchema.methods.generateEmailConfirmToken = function (next) {
